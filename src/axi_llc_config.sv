d44869c155a9a4da614c08a9802d3420a460c10a
Diyou Shen <dishen@student.ethz.ch>
2023-04-08 16:23:40 +0200

    Fix the bug for set-flushing and set NumLines to 32

--- src/axi_llc_config.sv
+++ src/axi_llc_config.sv
@@ -310,7 +310,7 @@ module axi_llc_config #(
   ////////////////////////
   // local address maps for bypass 1:Bypass 0:LLC
   rule_full_t [1:0] axi_addr_map_ar, axi_addr_map_aw;
-  localparam int unsigned num_set_flush_reg = $ceil(Cfg.NumLines / RegWidth);
+  localparam int unsigned num_set_flush_reg = ((Cfg.NumLines/RegWidth)==0) ? 1 : $ceil(Cfg.NumLines / RegWidth);
   localparam int unsigned flushed_set_length = num_set_flush_reg * RegWidth;
   logic       [flushed_set_length-1:0] conf_regs_i_flushed_set;
   logic       [flushed_set_length-1:0] conf_regs_i_cfg_flush_set;
@@ -318,19 +318,13 @@ module axi_llc_config #(
   assign num_unvalid_bit_flush_set = flushed_set_length - Cfg.NumLines;
   logic       [flushed_set_length-1:0] raw_cfg_flush_set, raw_flushed_set, mask_flush_set;
 /********************************************     SET BASED CACHE PARTITIONING     ********************************************/
-  assign raw_flushed_set = {conf_regs_i.flushed_set3,
-                            conf_regs_i.flushed_set2,
-                            conf_regs_i.flushed_set1,
-                            conf_regs_i.flushed_set0};
-  assign raw_cfg_flush_set = {conf_regs_i.cfg_flush_set3,
-                              conf_regs_i.cfg_flush_set2,
-                              conf_regs_i.cfg_flush_set1,
-                              conf_regs_i.cfg_flush_set0};
+  assign raw_flushed_set = {conf_regs_i.flushed_set0};
+  assign raw_cfg_flush_set = {conf_regs_i.cfg_flush_set0};
 /******************************************************************************************************************************/
 // If the user set the flush bit position of conf_regs_i.flushed_set* which is beyond the number of cache lines, those bits are ignored
   always_comb begin
-    mask_flush_set = 0;
-    mask_flush_set |= {Cfg.NumLines{1'b1}};
+    // mask_flush_set = 0;
+    mask_flush_set = {Cfg.NumLines{1'b1}};
   end
   assign conf_regs_i_flushed_set = raw_flushed_set & mask_flush_set;
   assign conf_regs_i_cfg_flush_set = raw_cfg_flush_set & mask_flush_set;
@@ -350,36 +344,7 @@ module axi_llc_config #(
   logic [MaxThread * Cfg.IndexLength - 1 : 0] conf_regs_i_cfg_set_partition;
 
 /********************************************     SET BASED CACHE PARTITIONING     ********************************************/
-  assign conf_regs_i_cfg_set_partition = {conf_regs_i.cfg_set_partition31[valid_reg_bit-1:0], 
-                                          conf_regs_i.cfg_set_partition30[valid_reg_bit-1:0], 
-                                          conf_regs_i.cfg_set_partition29[valid_reg_bit-1:0], 
-                                          conf_regs_i.cfg_set_partition28[valid_reg_bit-1:0], 
-                                          conf_regs_i.cfg_set_partition27[valid_reg_bit-1:0], 
-                                          conf_regs_i.cfg_set_partition26[valid_reg_bit-1:0], 
-                                          conf_regs_i.cfg_set_partition25[valid_reg_bit-1:0], 
-                                          conf_regs_i.cfg_set_partition24[valid_reg_bit-1:0], 
-                                          conf_regs_i.cfg_set_partition23[valid_reg_bit-1:0], 
-                                          conf_regs_i.cfg_set_partition22[valid_reg_bit-1:0], 
-                                          conf_regs_i.cfg_set_partition21[valid_reg_bit-1:0], 
-                                          conf_regs_i.cfg_set_partition20[valid_reg_bit-1:0], 
-                                          conf_regs_i.cfg_set_partition19[valid_reg_bit-1:0], 
-                                          conf_regs_i.cfg_set_partition18[valid_reg_bit-1:0], 
-                                          conf_regs_i.cfg_set_partition17[valid_reg_bit-1:0], 
-                                          conf_regs_i.cfg_set_partition16[valid_reg_bit-1:0], 
-                                          conf_regs_i.cfg_set_partition15[valid_reg_bit-1:0], 
-                                          conf_regs_i.cfg_set_partition14[valid_reg_bit-1:0], 
-                                          conf_regs_i.cfg_set_partition13[valid_reg_bit-1:0], 
-                                          conf_regs_i.cfg_set_partition12[valid_reg_bit-1:0], 
-                                          conf_regs_i.cfg_set_partition11[valid_reg_bit-1:0], 
-                                          conf_regs_i.cfg_set_partition10[valid_reg_bit-1:0], 
-                                          conf_regs_i.cfg_set_partition9[valid_reg_bit-1:0], 
-                                          conf_regs_i.cfg_set_partition8[valid_reg_bit-1:0], 
-                                          conf_regs_i.cfg_set_partition7[valid_reg_bit-1:0], 
-                                          conf_regs_i.cfg_set_partition6[valid_reg_bit-1:0], 
-                                          conf_regs_i.cfg_set_partition5[valid_reg_bit-1:0], 
-                                          conf_regs_i.cfg_set_partition4[valid_reg_bit-1:0], 
-                                          conf_regs_i.cfg_set_partition3[valid_reg_bit-1:0], 
-                                          conf_regs_i.cfg_set_partition2[valid_reg_bit-1:0], 
+  assign conf_regs_i_cfg_set_partition = {conf_regs_i.cfg_set_partition2[valid_reg_bit-1:0], 
                                           conf_regs_i.cfg_set_partition1[valid_reg_bit-1:0], 
                                           conf_regs_i.cfg_set_partition0[valid_reg_bit-1:0]};
 /******************************************************************************************************************************/
@@ -535,28 +500,16 @@ module axi_llc_config #(
     conf_regs_o.cfg_flush     = conf_regs_i.cfg_flush;
 /********************************************     SET BASED CACHE PARTITIONING     ********************************************/
     conf_regs_o.cfg_flush_set0 = conf_regs_i.cfg_flush_set0;
-    conf_regs_o.cfg_flush_set1 = conf_regs_i.cfg_flush_set1;
-    conf_regs_o.cfg_flush_set2 = conf_regs_i.cfg_flush_set2;
-    conf_regs_o.cfg_flush_set3 = conf_regs_i.cfg_flush_set3;
     conf_regs_o.commit_cfg    = conf_regs_i.commit_cfg;
     conf_regs_o.flushed       = conf_regs_i.flushed;
     conf_regs_o.flushed_set0   = conf_regs_i.flushed_set0;
-    conf_regs_o.flushed_set1   = conf_regs_i.flushed_set1;
-    conf_regs_o.flushed_set2   = conf_regs_i.flushed_set2;
-    conf_regs_o.flushed_set3   = conf_regs_i.flushed_set3;
     // Register enables
     conf_regs_o.cfg_spm_en    = 1'b1;   // default one
     conf_regs_o.cfg_flush_en  = 1'b1;   // default one
     conf_regs_o.cfg_flush_set0_en = 1'b1;
-    conf_regs_o.cfg_flush_set1_en = 1'b1;
-    conf_regs_o.cfg_flush_set2_en = 1'b1;
-    conf_regs_o.cfg_flush_set3_en = 1'b1;
     conf_regs_o.commit_cfg_en = 1'b0;   // default disabled
     conf_regs_o.flushed_en    = 1'b0;   // default disabled
     conf_regs_o.flushed_set0_en  = 1'b0;
-    conf_regs_o.flushed_set1_en  = 1'b0;
-    conf_regs_o.flushed_set2_en  = 1'b0;
-    conf_regs_o.flushed_set3_en  = 1'b0;
 /******************************************************************************************************************************/
 
     // Flush state machine
@@ -588,9 +541,6 @@ module axi_llc_config #(
         conf_regs_o.cfg_flush_en  = 1'b0;
 /********************************************     SET BASED CACHE PARTITIONING     ********************************************/
         conf_regs_o.cfg_flush_set0_en = 1'b0;
-        conf_regs_o.cfg_flush_set1_en = 1'b0;
-        conf_regs_o.cfg_flush_set2_en = 1'b0;
-        conf_regs_o.cfg_flush_set3_en = 1'b0;
 /******************************************************************************************************************************/
         llc_isolate_o             = 1'b0;
         // Change state, if there is a flush request, i.e. CommitCfg was set
@@ -624,9 +574,6 @@ module axi_llc_config #(
 
 /********************************************     SET BASED CACHE PARTITIONING     ********************************************/
             conf_regs_o.cfg_flush_set0 = 64'b0;
-            conf_regs_o.cfg_flush_set1 = 64'b0;
-            conf_regs_o.cfg_flush_set2 = 64'b0;
-            conf_regs_o.cfg_flush_set3 = 64'b0;
 /******************************************************************************************************************************/
           end else begin
             flush_state_d = FsmSendFlush;
@@ -725,31 +672,16 @@ module axi_llc_config #(
             // reset the flushed register to SPM as new requests can enter the cache
 /********************************************     SET BASED CACHE PARTITIONING     ********************************************/
             conf_regs_o.flushed_set0    = 64'b0; /**************** temperarily not used since SPM is way-based ****************/ 
-            conf_regs_o.flushed_set1    = 64'b0; /**************** temperarily not used since SPM is way-based ****************/ 
-            conf_regs_o.flushed_set2    = 64'b0; /**************** temperarily not used since SPM is way-based ****************/ 
-            conf_regs_o.flushed_set3    = 64'b0; /**************** temperarily not used since SPM is way-based ****************/ 
             conf_regs_o.flushed_set0_en = 1'b1; 
-            conf_regs_o.flushed_set1_en = 1'b1; 
-            conf_regs_o.flushed_set2_en = 1'b1; 
-            conf_regs_o.flushed_set3_en = 1'b1; 
             to_flush_set_d              = set_t'(1'b0);
             // Clear the `CfgFlushSet` register, load enable is default '1
             conf_regs_o.cfg_flush_set0  = 64'b0;
-            conf_regs_o.cfg_flush_set1  = 64'b0;
-            conf_regs_o.cfg_flush_set2  = 64'b0;
-            conf_regs_o.cfg_flush_set3  = 64'b0;
           end else begin
             // there are still cache lines to flush
             flush_state_d = FsmInitFlush;
             conf_regs_o_flushed_set = conf_regs_i_flushed_set | flush_set_ind;
             conf_regs_o.flushed_set0 = conf_regs_o_flushed_set[1*RegWidth-1:0*RegWidth];
-            conf_regs_o.flushed_set1 = conf_regs_o_flushed_set[2*RegWidth-1:1*RegWidth];
-            conf_regs_o.flushed_set2 = conf_regs_o_flushed_set[3*RegWidth-1:2*RegWidth];
-            conf_regs_o.flushed_set3 = conf_regs_o_flushed_set[4*RegWidth-1:3*RegWidth];
             conf_regs_o.flushed_set0_en = 1'b1;
-            conf_regs_o.flushed_set1_en = 1'b1;
-            conf_regs_o.flushed_set2_en = 1'b1;
-            conf_regs_o.flushed_set3_en = 1'b1;
 /******************************************************************************************************************************/
           end
         end else begin
