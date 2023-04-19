a66cfc38b394b813b028e15c34dde99c493901b2
Diyou Shen <dishen@student.ethz.ch>
2023-04-19 16:57:33 +0200

    Fix instantiation typo for SRAM Macro

--- src/hit_miss_detect/axi_llc_tag_store.sv
+++ src/hit_miss_detect/axi_llc_tag_store.sv
@@ -328,9 +328,9 @@ module axi_llc_tag_store #(
       .req_i   ( ram_req[i] ),
       .we_i    ( ram_we[i]  ),
       .addr_i  ( ram_index  ),
-      .wdata_i ( ram_wdata  ),
+      .wdata_i ( sram_wdata  ),
       .be_i    ( ram_we[i]  ),
-      .rdata_o ( ram_rdata  )
+      .rdata_o ( sram_rdata  )
     );
 
 
